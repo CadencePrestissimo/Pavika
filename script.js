0fa4fc287142fe58a0b954d14ac5f8b3266a9ed3
pavikachaudhary <iit2019011@iiita.ac.in>
2020-05-26 13:33:56 +0530

    my web

---
+++ script.js
@@ -0,0 +1,13 @@
+<script>
+function openForm() {
+document.getElementById("myForm").style.display = "block";
+document.getElementById("but").style.display="none";
+document.getElementById("space").style.display="none";
+}
+
+function closeForm() {
+document.getElementById("myForm").style.display = "none";
+  document.getElementById("but").style.display="";
+    document.getElementById("space").style.display="";
+}
+</script>
